2a4814d619408b32f2af53fb8d0d7089bbbffce8
Santi Sekuens <santipbr@gmail.com>
2025-08-26 08:47:20 +0200

    Elimina vistas antiguas

--- eticco_superset_integration/models/__init__.py
+++ eticco_superset_integration/models/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import superset_utils
 from . import res_config_settings
-from . import superset_dashboard_selector
-from . import superset_dashboard_viewer
 from . import superset_analytics_hub
